b1c393ce6a78da093f3c0486133f3a78b74944e6
Karol Wojslaw <karol.wojslaw@globallogic.com>
2024-12-09 15:43:43 +0100

    Add solutions to chapter 5. challanges

--- Ch05/05_03/nearest.c
+++ Ch05/05_03/nearest.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-void main(){
-  double hours=3.1, age=14.8;
-  
-  printf("I parked for %g hours, so I got %g hours charged.\n",hours,/*fill in*/);
+void main()
+{
+  double hours = 3.1, age = 14.8;
 
-  printf("Jake is %g years old, so he's %g.\n\n",age,/*fill in*/);
-  
+  printf("I parked for %g hours, so I got %g hours charged.\n", hours, ceil(hours));
+
+  printf("Jake is %g years old, so he's %g.\n\n", age, floor(age));
+
   printf("I parked for %g hours, so I stayed a little longer than %g hours.\n",
-          hours,/*fill in*/);
-  
-  printf("Jake is %g years old, so he's practically %g.\n",age,/*fill in*/);
-  
-  double x=-5.6;
+         hours, trunc(hours));
+
+  printf("Jake is %g years old, so he's practically %g.\n", age, round(age));
+
+  double x = -5.6;
   printf("x = %g, ceil(x)=%g, floor(x)=%g, trunc(x)=%g, round()x.=%g\n",
-          x,ceil(x),floor(x),trunc(x),round(x));
+         x, ceil(x), floor(x), trunc(x), round(x));
 }
